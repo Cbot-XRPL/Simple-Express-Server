ff0b14cb4a25acec67a1519b7143cf597950abbd
Cbot-XRPL <cybrcris@gmail.com>
2025-01-11 00:57:44 -0500

    cnage

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.use('/', express.static('../websites/labs'))
+app.use('/', express.static('../websites/cbot-labs'))
 
 app.listen(8081)
